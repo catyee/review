ca5d6f566a950d06c1464f0c3122de46efd7a4c7
catyee <526379113@qq.com>
2020-05-20 16:03:55 +0800

    update promise

--- js/promise/promise3.js
+++ js/promise/promise3.js
@@ -1,71 +1,63 @@
-import { reject } from "./promise2";
-
-const PENDING = 'pending';
+const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 function Promise(executor) {
     let self = this
     self.status = PENDING
     self.value = undefined
-    self.onResolvedCallbacks = []
+    self.onFulfilledCallbacks = []
     self.onRejectedCallbacks = []
     function resolve(value) {
-        if(value !== null && value.then && typeof value.then === 'function') {
-            return value.then(resolve, reject)
+        // todo..............................
+        if(value != null && value.then && typeof value.then === 'function'){
+           return value.then(resolve,reject)
         }
         setTimeout(function() {
             if(self.status === PENDING) {
                 self.status = FULFILLED
                 self.value = value
-                self.onResolvedCallbacks.forEach(function(cb) {
-                    cb(self.value)
-                })
+                self.onFulfilledCallbacks.forEach(cb => cb(self.value))
             }
         })
     }
-    function reject(reason) {
+    function reject(value) {
         setTimeout(function() {
             if(self.status === PENDING) {
                 self.status = REJECTED
-                self.value = reason
-                self.onRejectedCallbacks.forEach(function(cb) {
-                    cb(self.value)
-                })
+                self.value = value
+                self.onRejectedCallbacks.forEach(cb => cb(self.value))
             }
-        });
+        })
     }
     try{
-        executor(resolve, reject)
+        executor(resolve,reject)
     }catch(e) {
         reject(e)
     }
-
 }
-function resolvePromise(promise2, x, resolve, reject) {
+function resolvePromise(promise2, x, resolve,reject) {
     if(promise2 === x) {
         return reject(new TypeError('循环引用'))
     }
-    let called = false // promise2是否已经resolve或reject了
     if(x instanceof Promise) {
         if(x.status === PENDING) {
-            x.then(function(y){
+            x.then(function(y) {
                 resolvePromise(promise2,y,resolve,reject)
-            }, reject)
-        }else {
-            x.then(resolve, reject)
+            },reject)
+        }else{
+            x.then(resolve,reject)
         }
-    }else if(x !== null && ((typeof x === 'object' || typeof x === 'function'))) {
+    }else if(x && ((typeof x === 'function') || (typeof x === 'object'))) {
+        let called = false
+        let then = x.then;
         try{
-            let then = x.then
             if(typeof then === 'function') {
+                if(called) return
+                called = true
                 then.call(x, function(y) {
-                    if(called) return
-                    called = true
                     resolvePromise(promise2,y,resolve,reject)
-                }, function(err) {
-                    if(called) return
-                    called = true
-                    reject(err)
+                }, function(e) {
+                    reject(e)
                 })
             }else{
                 resolve(x)
@@ -75,7 +67,41 @@ function resolvePromise(promise2, x, resolve, reject) {
             called = true
             reject(e)
         }
-    }else{
+    }else {
         resolve(x)
+    }
+}
+Promise.prototype.then = function(onFulfilled, onRejected) {
+    let self = this
+    let promise2
+    typeof onFulfilled === 'function' ? onFulfilled : function(value) {return value}
+    typeof onRejected === 'function' ? onRejected : function(e) {throw e}
+    if(self.status === FULFILLED) {
+        return promise2 = new Promise(function(resolve,reject) {
+            setTimeout(function() {
+                let x = onFulfilled(self.value)
+                resolvePromise(promise2,x,resolve, reject)
+            })
+        })
+    }
+    if(self.status === REJECTED) {
+        return promise2 = new Promise(function(resolve,reject) {
+            setTimeout(function() {
+                let x = onRejected(self.value)
+                resolvePromise(promise2,x,resolve, reject)
+            })
+        })
+    }
+    if(self.status === PENDING) {
+        return promise2 = new Promise(function(resolve,reject) {
+            self.onFulfilledCallbacks.push(function() {
+                let x = onFulfilled(self.value)
+                resolvePromise(promise2,x,resolve, reject)
+            })
+            self.onRejectedCallbacks.push(function() {
+                let x = onRejected(self.value)
+                resolvePromise(promise2,x,resolve, reject)
+            })
+        })
     }
 }
